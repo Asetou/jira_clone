9188bbae5aec8875f88b5ad3a8d23e529f6f9afb
Panya Seth Ou <122097534+Asetou@users.noreply.github.com>
2025-01-16 22:33:14 -0800

    updated buttons tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
 import { Button } from "@/components/ui/button";
 
+
 export default function Home() {
   return (
-    <div>
-      Hello World
-      <p className="text-red-500">Panya</p>
+    <div className= "flex gap-4">
+      <input/>
+      <Button size="lg">
+        Primary
+      </Button>
+      
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      
+      <Button variant="destructive">
+        Desstructive
+      </Button>
+      
+      <Button variant="ghost">
+        ghost
+      </Button>
+      <Button variant="muted">
+        muted
+      </Button>
+      <Button variant="teritrary">
+        teritrary
+      </Button>
+
     </div>
   );
 }
